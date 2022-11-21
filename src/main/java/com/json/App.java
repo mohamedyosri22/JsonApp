1698584961ce0093ac6c168206c022e5f150d08c
mhmdyouali <mhmd.you@yahoo.com>
2022-11-22 00:28:27 +0200

    ObjectMapper

--- src/main/java/com/json/App.java
+++ src/main/java/com/json/App.java
@@ -1,13 +1,22 @@
 package com.json;
 
+import java.io.File;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.json.model.User;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
+		ObjectMapper map = new ObjectMapper();
+		User user = map.readValue(new File("info/user.json"), User.class);
+		System.out.println(user.getId() + "  " + user.getName());
+		System.out.println(user.getAge() + "  " + user.getAddress()[0]+","+user.getAddress()[1]);
+	}
 }
